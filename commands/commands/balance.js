3c2393df1192b304cd94854d9c51d9537bb70feb
frontman880 <leduynamj97@gmail.com>
2026-01-12 10:42:12 +0700

    Delete commands/commands directory

--- commands/commands/balance.js
+++
@@ -1,8 +0,0 @@
-const { getUser } = require('../utils/economy');
-
-module.exports = {
-  async execute(message) {
-    const user = getUser(message.author.id);
-    message.reply(`💰 Số dư của bạn: ${user.balance} coins`);
-  }
-};
